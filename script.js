6dbf71d29d949786828ddd71c1e055b95edbcc08
FCsab <161705557+FCsab@users.noreply.github.com>
2024-10-02 12:13:46 +0200

    yea

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 const main = document.getElementById('main');
 const header = document.getElementById('header');
 const footer = document.getElementById('footer');
-let footerText=document.createElement('p');
-footerText.innerHTML = 'GitHub: <a href="https://github.com/FCsab/weblap"></a>';
+
+let footerDiv = document.createElement('div');
+let footerText = document.createElement('p');
+footerText.innerHTML = '<a href="https://github.com/FCsab/weblap">GitHub</a>';
+
+footerDiv.style.display = 'flex';
+footerDiv.style.justifyContent = 'center';
+footerDiv.style.alignItems = 'center';
+footerDiv.style.height = '100%';
+
+footerDiv.appendChild(footerText);
+footer.appendChild(footerDiv);
