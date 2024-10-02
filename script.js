5e24db63fc2771c1764f00784c09000d10b9a6fa
FCsab <161705557+FCsab@users.noreply.github.com>
2024-10-02 12:08:12 +0200

    setup

---
+++ script.js
@@ -0,0 +1,5 @@
+const main = document.getElementById('main');
+const header = document.getElementById('header');
+const footer = document.getElementById('footer');
+let footerText=document.createElement('p');
+footerText.innerHTML = 'GitHub: <a href="https://github.com/FCsab/weblap"></a>';
